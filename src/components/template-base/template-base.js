f02fdb6dce31d3d6b30fe2e1467d5f5bac81297c
James Humphreys <james@binaryjim.co.uk>
2021-01-11 14:23:42 +0000

    Added button to enable tina CMS editing mode

--- src/components/template-base/template-base.js
+++ src/components/template-base/template-base.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Head from "next/head";
+import { useCMS } from "tinacms";
 import PropTypes from "prop-types";
 
 const TemplateBase = ({ children }) => {
+  const cms = useCMS();
+
+  const handleOnEnableCMSClick = () => cms.enable();
+
   return (
     <React.Fragment>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {children}
+      <div className="container">
+        {children}
+        {cms.disabled && (
+          <button className="margin1px" onClick={handleOnEnableCMSClick}>
+            Enable CMS
+          </button>
+        )}
+      </div>
     </React.Fragment>
   );
 };
